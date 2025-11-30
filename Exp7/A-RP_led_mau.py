93577879e815cc2e967f0afe6bedfdc16dcf5f9e
ManiMauSaglyanaKhau <rajlakshmidesai78@gmail.com>
2025-11-30 14:37:09 +0530

    Add files via upload

---
+++ Exp7/A-RP_led_mau.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+led_pins = [17, 22, 27]
+
+for pin in led_pins:
+    GPIO.setup(pin, GPIO.OUT)
+
+while True:
+    for pin in led_pins:
+        GPIO.output(pin, GPIO.HIGH)
+        sleep(1)
+    for pin in led_pins:
+        GPIO.output(pin, GPIO.LOW)
+        sleep(1)
