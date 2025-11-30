70d1085efa5eb93f34c3137f7d781af6dcce1ada
ash3434e <153093589+ash3434e@users.noreply.github.com>
2025-11-30 19:08:58 +0530

    Add files via upload

---
+++ Exp7/7B_IoT.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as GPIO  
+import me  
+import urllib.request  
+import Adafruit_DHT  
+ 
+GPIO.setmode(GPIO.BCM)  
+pin = 22  
+ 
+while True:  
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, pin)  
+    if humidity is not None and temperature is not None:  
+        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))  
+    else:  
+        print('Failed to get reading. Try again!')  
+    me.sleep(2)
